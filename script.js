6a660f61e95be6d4cd217379670a30f219c216c5
Victor Volkov <vityan2504@gmail.com>
2023-11-16 16:12:05 +0300

    Реализовал смену цвета квадрата при нажатии на кнопку, автоматическое изменение размера круга при изменении ползунка и скрыл кнопку внутри круга

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+'use strict';
 
+const btn = document.getElementById('btn');
+const input = document.getElementById('text');
+const square = document.getElementById('square');
+const circle = document.getElementById('circle');
+const e_btn = document.getElementById('e_btn');
+const range = document.getElementById('range');
 
+const changeText = function(e) {
+   if (input.value !== "") square.style.backgroundColor = input.value;
+   input.value = "";
+};
 
+const changeSize = function(e) {
+   circle.style.height = e.target.value + '%';
+   circle.style.width = e.target.value + '%';
+}
 
+btn.addEventListener('click', changeText);
+range.addEventListener('input', changeSize);
 
+e_btn.style.display = 'none';
